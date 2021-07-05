315332c346ccde293970fe3bf50d5238e3f151e1
Aylin Artut <qcural@t-online.de>
2021-07-05 02:08:19 +0200

    Removed target directory & added to gitIgnore-file.

--- target/SoftwareEngineeringI_Parkhaus-1.0-SNAPSHOT/index.js
+++
@@ -1,9 +0,0 @@
-$( function() {
-    $( "#car" ).draggable();
-    $( "#carPark" ).droppable({
-        drop: function( event, ui ) {
-            $( this )
-            $('form').submit();
-        }
-    });
-} );
